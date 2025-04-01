ec606127ace0ab4c190758fbb7832f1134524f90
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-04-01 11:00:22 +0630

    learning blazor crud

---
+++ MovieDemoWithBlazor/Data/MovieDemoWithBlazorContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieDemoWithBlazor.Model;
+
+namespace MovieDemoWithBlazor.Data
+{
+    public class MovieDemoWithBlazorContext : DbContext
+    {
+        public MovieDemoWithBlazorContext (DbContextOptions<MovieDemoWithBlazorContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieDemoWithBlazor.Model.Movie> Movie { get; set; } = default!;
+    }
+}
